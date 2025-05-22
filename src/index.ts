6aefe28cc462f35fb5e90f801e5e92d322fff2b0
Astra <abhaypratapsinghwd@gmail.com>
2025-05-22 10:45:44 +0530

    Added redisConnection to /

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
+import redisClient from "./lib/redis.js";
 import cleanRoutes from "./routes/clean.js";
 import queueRoutes from "./routes/queue.js";
 
@@ -11,9 +12,23 @@ app.use(express.json());
 app.use("/api/queue", queueRoutes);
 app.use("/api/clean", cleanRoutes);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   console.log("Request made to / endpoint");
-  res.status(200).json({ message: "Service is up" });
+  try {
+    const pong = await redisClient.ping();
+    res.status(200).json({
+      message: "Service is up",
+      redis: pong,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("error.stack is ", error.stack);
+      console.log("error.message is ", error.message);
+    }
+    res.status(500).json({
+      message: "Service is up, but Redis connection failed",
+    });
+  }
 });
 
 app.listen(PORT, () => {
